9736462f3ad5d6e470a0a0b91f897742a6886a6d
Andres Jimenez <andresje17@gmail.com>
2021-02-01 20:24:26 -0600

    Add mdx testing functionality
    The works component renders the mdx files dynamically

---
+++ src/components/works/works.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+// It requires and returns all modules that match
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+// It contains references to all modules in that ./content directory
+const mdxContextModule = requireAll(
+  require.context(
+    '!babel-loader!@mdx-js/loader!./content',
+    false,
+    /^\.\/.*\.mdx$/
+  )
+);
+
+// It extracts the MDX component and creates an object to create children in JSX
+const mdxComponents = mdxContextModule.map((mdx, index) => {
+  return { key: index, component: mdx.default }
+})
+
+console.log(mdxComponents);
+
+// It renders every module in the ./content directory
+class Item extends React.Component {
+  render() {
+    const MdxComponent = this.props.component;
+    return <div><MdxComponent /></div>;
+  }
+}
+
+Item.propTypes = {
+  component: PropTypes.func,
+}
+
+export default class Works extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        { mdxComponents.map((mdx) => <Item key={mdx.key} component={mdx.component} />)}
+      </div>
+    )
+  }
+}
