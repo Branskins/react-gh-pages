77ab20db297255288394c18a112d9bdd5dfd89b3
Andres Jimenez <andresje17@gmail.com>
2021-02-16 23:32:00 -0600

    Add work components
    - Some components to see individual works functionality

---
+++ src/components/works/content/days-malayalam.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DaysMalayalam extends Component {
+  render() {
+    return (
+      <div>
+        <h1>36 Days of Malayalam type</h1>
+      </div>
+    )
+  }
+}
---
+++ src/components/works/content/vibrant-portraits.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class VibrantPortraits extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Vibrant Portraits</h1>
+      </div>
+    )
+  }
+}
